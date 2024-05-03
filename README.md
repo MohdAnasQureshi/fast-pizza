1e132b1d46cbc2b11856acfeab34159cf75f1004
MohdAnasQureshi <qureshianas471@gmail.com>
2024-05-03 22:29:22 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Wild Oasis
+
+## Fast Pizza Co. is an pizza delivering app fontend prototype with functionalities such as add pizza, delete pizza, add to cart, order pizza
---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,24 @@
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
+
+function Menu() {
+  //3. use data returned by loader( )
+  const menu = useLoaderData();
+
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+// 1. declare loader function
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
